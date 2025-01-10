6fa54f42a599d5897d20b95f58269d18e191c4d8
madhu <madhukush006@gmail.com>
2025-01-10 17:47:58 +0530

    Add Forgot Password functionality

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from authentication.manager import CustomUserManager
+from django.utils.timezone import now
 
 
 class TimestampedModel(models.Model):
@@ -11,15 +12,26 @@ class TimestampedModel(models.Model):
         abstract = True
 
 
-class User(AbstractUser, TimestampedModel):  #extend AbstractUser to retain the default features while adding your own
-    username = None    
+class User(
+    AbstractUser, TimestampedModel
+):  # extend AbstractUser to retain the default features while adding your own
+    username = None
+    full_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
+    is_email_verified = models.BooleanField(default=False)
+    email_verified_sent_at = models.DateTimeField(blank=True, null=True)
+    email_verified_token = models.CharField(max_length=100, blank=True, null=True)
+    reset_password = models.BooleanField(default=False)
+    forgot_password_token = models.CharField(max_length=100, blank=True, null=True)
+    forgot_password_sent_at = models.DateTimeField(blank=True, null=True)
 
     # Custom manager
-    objects = CustomUserManager()   #CustomUserManager is a custom class (defined separately) that provides methods for creating both regular users and superusers.
+    objects = (
+        CustomUserManager()
+    )  # CustomUserManager is a custom class (defined separately) that provides methods for creating both regular users and superusers.
 
-    USERNAME_FIELD = 'email'  # Use email as the username field
-    
+    USERNAME_FIELD = "email"  # Use email as the username field
+
     REQUIRED_FIELDS = []
 
     def __str__(self):
@@ -28,6 +40,7 @@ class User(AbstractUser, TimestampedModel):  #extend AbstractUser to retain the 
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
@@ -36,22 +49,28 @@ class Blogpost(TimestampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="blogs", null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="blogs", null=True, blank=True
+    )
     likes = models.ManyToManyField(User, related_name="like_blogs", blank=True)
 
     def total_like(self):
         return self.likes.count()
- 
+
     def __str__(self):
         return self.title
 
 
 class Comment(TimestampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog = models.ForeignKey(Blogpost, on_delete=models.CASCADE, related_name="comments")
+    blog = models.ForeignKey(
+        Blogpost, on_delete=models.CASCADE, related_name="comments"
+    )
     content = models.TextField()
 
 
 class Like(TimestampedModel):
-    blog_post = models.ForeignKey(Blogpost, on_delete=models.CASCADE, related_name="like")
+    blog_post = models.ForeignKey(
+        Blogpost, on_delete=models.CASCADE, related_name="like"
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- authentication/utils.py
+++ authentication/utils.py
@@ -1,19 +1,40 @@
-from django.core.signing import Signer, BadSignature
+import uuid
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.conf import settings
 
 
-signer = Signer()
+def generate_verification_token():
+    return str(uuid.uuid4())
 
 
-def generate_verification_token(name):
-    return signer.sign(name)
+def generate_password_token():
+    return str(uuid.uuid4())
 
 
-def verify_token(token):
-    print(f"Received Token: {token}") 
-    try:
-        email = signer.unsign(token)
-        print(email)
-        return email
-    except BadSignature:
-        print("BadSignature: Token is invalid or tampered.")
-        return None
+def send_forgot_password(user):
+    token = user.forgot_password_token
+    verification_url = reverse("password_reset_done", kwargs={"token": token})
+    full_url = f"{settings.SITE_URL}{verification_url}"
+    print(f"Verification URL: {full_url}")
+    subject = "Reset Password"
+    message = f"""
+    Hi {user.get_full_name()}, 
+    Click the link to verify your email and reset your password:
+    {full_url}
+    This link is valid for 5 minutes.
+    """
+    from_email = settings.DEFAULT_FROM_EMAIL
+    send_mail(subject, message, from_email, [user.email])
+
+
+def send_verification_email(user):
+    # name = user.full_name[:2]
+    token = user.email_verified_token
+    verification_url = f"{settings.SITE_URL}/verify-email/?token={token}"
+    subject = "Verify your email address"
+    message = (
+        f"hi {user.email}, click the link to verify your email: {verification_url}"
+    )
+    from_email = settings.DEFAULT_FROM_EMAIL
+    send_mail(subject, message, from_email, [user.email])
